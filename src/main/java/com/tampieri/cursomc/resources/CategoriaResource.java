ea895f48cdbc5295e34759b0a822801158006066
higortampieri@gmail.com <higrtampieri@gmail.com>
2018-08-21 16:12:30 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/tampieri/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tampieri/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,5 @@
 package com.tampieri.cursomc.resources;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +19,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Optional<Categoria> obj = service.buscar(id);		
+		Categoria obj = service.buscar(id);		
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/tampieri/cursomc/services/CategoriaService.java
+++ src/main/java/com/tampieri/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.tampieri.cursomc.domain.Categoria;
 import com.tampieri.cursomc.repositories.CategoriaRepository;
+import com.tampieri.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,8 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Optional<Categoria> buscar(Integer id) {
+	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj;
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
