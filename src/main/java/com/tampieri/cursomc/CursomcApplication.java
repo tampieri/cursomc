4ddfd021a0685dc023ab91c5314619f579a74c2d
higortampieri@gmail.com <higrtampieri@gmail.com>
2018-08-22 11:32:36 -0300

    Pedido, estadoPagamento e Pagamento

--- src/main/java/com/tampieri/cursomc/CursomcApplication.java
+++ src/main/java/com/tampieri/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.tampieri.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.tampieri.cursomc.domain.Cidade;
 import com.tampieri.cursomc.domain.Cliente;
 import com.tampieri.cursomc.domain.Endereco;
 import com.tampieri.cursomc.domain.Estado;
+import com.tampieri.cursomc.domain.Pagamento;
+import com.tampieri.cursomc.domain.PagamentoComBoleto;
+import com.tampieri.cursomc.domain.PagamentoComCartao;
+import com.tampieri.cursomc.domain.Pedido;
 import com.tampieri.cursomc.domain.Produto;
+import com.tampieri.cursomc.domain.enums.EstadoPagamento;
 import com.tampieri.cursomc.domain.enums.TipoCliente;
 import com.tampieri.cursomc.repositories.CategoriaRepository;
 import com.tampieri.cursomc.repositories.CidadeRepository;
 import com.tampieri.cursomc.repositories.ClienteRepository;
 import com.tampieri.cursomc.repositories.EnderecoRepository;
 import com.tampieri.cursomc.repositories.EstadoRepository;
+import com.tampieri.cursomc.repositories.PagamentoRepository;
+import com.tampieri.cursomc.repositories.PedidoRepository;
 import com.tampieri.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 
 	
@@ -87,6 +99,22 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 
 	}
---
+++ src/main/java/com/tampieri/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,36 @@
+package com.tampieri.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.tampieri.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+	}
+	
+	
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+
+	public PagamentoComCartao(Integer numeroDeParcelas) {
+		super();
+		
+	}
+
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+}
