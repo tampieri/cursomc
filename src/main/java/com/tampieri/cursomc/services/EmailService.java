383c608489ffad97cf1299fb1555eee89d46f145
higortampieri@gmail.com <higrtampieri@gmail.com>
2018-08-28 16:12:26 -0300

    Email Html

--- src/main/java/com/tampieri/cursomc/services/EmailService.java
+++ src/main/java/com/tampieri/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.tampieri.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.tampieri.cursomc.domain.Pedido;
@@ -8,5 +10,8 @@ public interface EmailService {
 	
  	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 
 }
