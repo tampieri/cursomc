638c69150e064744454fe34259e600a0d47ce819
higortampieri@gmail.com <higrtampieri@gmail.com>
2018-09-03 16:30:51 -0300

    Tratando exceções adequadamente

--- src/main/java/com/tampieri/cursomc/services/S3Service.java
+++ src/main/java/com/tampieri/cursomc/services/S3Service.java
@@ -21,6 +21,7 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.tampieri.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -40,7 +41,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO:" + e.getMessage());
+			throw new FileException("Erro de IO:" + e.getMessage());
 		}
 	}
 
@@ -53,7 +54,7 @@ public class S3Service {
 			LOG.info("Upload finalizado");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 
