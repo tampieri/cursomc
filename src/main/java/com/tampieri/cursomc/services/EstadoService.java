a4483d542383297b40f48ed0698114af1e4b9589
higortampieri@gmail.com <higrtampieri@gmail.com>
2018-09-18 14:04:09 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/tampieri/cursomc/services/EstadoService.java
@@ -0,0 +1,22 @@
+package com.tampieri.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tampieri.cursomc.domain.Estado;
+import com.tampieri.cursomc.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+	
+	@Autowired
+	private EstadoRepository repo;
+	
+	public List<Estado> findAll() {
+		return repo.findAllByOrderByNome();
+	}
+	
+	
+}
